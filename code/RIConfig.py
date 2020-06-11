08b6467a133fedc143deb7457a91f9d350ab3391
Bhuvaneshwari S <bhuvana_venky@yahoo.co.in>
2020-06-11 10:29:17 +0530

    Initial files commit

---
+++ code/RIConfig.py
@@ -0,0 +1,5 @@
+""" Python configuration file """
+INPUT_AGRSALES = '../data/inputs/aggregated_sales.csv'
+INPUT_INCIDENTS = '../data/inputs/incidents.csv'
+INPUT_LOCATIONS = '../data/inputs/locations.txt'
+INPUT_SALESFOLDER = '../data/inputs/sales/'
---
+++ code/RIUtils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import pandas as pd
+import RIConfig as cfg
+import warnings
+warnings.filterwarnings("ignore")
